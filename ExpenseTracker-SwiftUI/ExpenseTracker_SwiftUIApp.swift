7b73243bcdd35cd69af19855a7f485f07bf2c6f3
Gurpreet Kaur <gurpreet.dhillon03@gmail.com>
2024-01-30 23:49:35 -0800

    Initial Commit

---
+++ ExpenseTracker-SwiftUI/ExpenseTracker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTracker_SwiftUIApp.swift
+//  ExpenseTracker-SwiftUI
+//
+//  Created by ReetDhillon on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTracker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
