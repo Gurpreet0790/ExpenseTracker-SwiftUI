b00163e6ecbb2215dc7fd05717b5dcf90d09636b
Gurpreet Kaur <gurpreet.dhillon03@gmail.com>
2024-02-02 23:32:04 -0800

    Changes in Transaction Model

--- ExpenseTracker-SwiftUI/View/RecentTransactionsList.swift
+++ ExpenseTracker-SwiftUI/View/RecentTransactionsList.swift
@@ -37,10 +37,11 @@ struct RecentTransactionsList: View {
             
             
             //MARK: - Recent Transactions List
-            ForEach(transactionListVM.transactions.prefix(5)) { transaction in
+            ForEach(Array(transactionListVM.transactions.prefix(5).enumerated()), id: \.element) { index, transaction in
                 TransactionRow(transaction: transaction)
                 
                 Divider()
+                    .opacity(index == 4? 0: 1)
             }
         }
         .padding()
